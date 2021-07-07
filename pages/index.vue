fdeabcea6d963d7da1d087dfa7ddf754f07f0f6a
Razunter <Razunter@gmail.com>
2021-07-07 14:29:53 +0400

    sort button

--- pages/index.vue
+++ pages/index.vue
@@ -11,18 +11,8 @@
 
         <md-list>
           <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
+            <md-icon>reorder</md-icon>
+            <span class="md-list-item-text">Sort</span>
           </md-list-item>
 
           <md-list-item @click="saveBuilds">
