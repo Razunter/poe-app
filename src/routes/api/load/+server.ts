2f44da6df041367d22e874e3e34742faff922a0b
Razunter <Razunter@gmail.com>
2022-08-16 17:29:14 +0400

    new Svelte routing + pckg upgrade

--- src/routes/api/load/+server.ts
+++ src/routes/api/load/+server.ts
@@ -49,5 +49,6 @@ export const GET = async () => {
     response.body = Object.assign(buildsData, versionsData)
   }
 
+  throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292701)");
   return response
 }
